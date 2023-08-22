81dd154fe799181c4d23150a4e3ecd40095bfb42
lanpokn <2606629917@qq.com>
2023-08-22 20:41:34 +0800

    add multi spectral to the UE and normal sensor, note that you need to copy/generate your own opencv dll

--- IEBCS-main/cpp/utils/setup_display.py
+++ IEBCS-main/cpp/utils/setup_display.py
@@ -2,24 +2,19 @@ from setuptools import setup, Extension
 import numpy
 import cv2
 
+# These paths should be changed to your own paths
 module = Extension("sgrb2lum",
-                   sources=["/home/lanpokn/Documents/2022/pbrt/pbrt-v4/pbrt-v4/IEBCS-main/cpp/utils/display.cpp"],
-                #    include_dirs=[numpy.get_include(), cv2.get_include()],
-                   include_dirs=[numpy.get_include(),"/usr/include/opencv4"],
-                   library_dirs = ["/usr/lib/x86_64-linux-gnu","/usr/local/lib"],
-                   libraries=["opencv_features2d", 
-                           "opencv_highgui", "opencv_core"],
-                   extra_compile_args=["-std=c++11"],
-                   extra_link_args=["-std=c++11"])
+                   sources=["C:/Users/admin/Documents/2023/PBES/my_pbrt/IEBCS-main/cpp/utils/display.cpp"],
+                   include_dirs=[numpy.get_include(), "C:/Users/admin/Desktop/code/opencv/build/include/"],
+                   library_dirs=["C:/Users/admin/Desktop/code/opencv/build/x64/vc16/bin/", "C:/Users/admin/Desktop/code/opencv/build/x64/vc16/lib/", "C:/Users/admin/Desktop/code/opencv/build/x64/vc16/"],
+                   libraries=["opencv_world470d"],
+                  #  extra_compile_args=["/std:c++11"],
+                  #  extra_link_args=["/NODEFAULTLIB:MSVCRT", "/NODEFAULTLIB:LIBCMT", "/MT"],
+                   language="c++")
 
 setup(
     name="sgrb2lum",
     version="1.0",
     description="Converts SRGB to luminance",
-    ext_modules=[module],
-    # cmdclass={"build_ext": build_ext},
+    ext_modules=[module]
 )
-
-# import cv2
-# print(cv2.__version__)
-# print(cv2.getBuildInformation())
