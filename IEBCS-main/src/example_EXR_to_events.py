3afff7d8cf13106757e77d01160eda5af9aab49b
lanpokn <2606629917@qq.com>
2023-10-26 20:42:39 +0800

    clarify each param's effect, later should choose for each light condition and scene

--- IEBCS-main/src/example_EXR_to_events.py
+++ IEBCS-main/src/example_EXR_to_events.py
@@ -13,13 +13,19 @@ import dsi
 import numpy as np
 from ExrRead import read_exr_channel
 # filename = "C:/Users/hhq/Documents/blender/PBES_small/rotate_360/0000-0060.mkv"
-#why read single per time would have strong shadow?
 
+#lower lat will get more active pixel(remain event)
 lat = 100
+# higher jit will get blurer pixel and more active pixel , very obvious
 jit = 10
+#lower ref, higher active
 ref = 100
-tau = 300
+#it's the trans func, higher tau , lowwer the event gen
+tau = 3
+#just threshould, lower it can let it more realistic(not straight event)
+#very obvious!
 th = 0.3
+#higher it, more active is
 th_noise = 0.01
 # cap = cv2.VideoCapture(filename)
 #only first is hw, other are wh
@@ -28,14 +34,16 @@ height = 720
 dsi.initSimu(int(height), int(width))
 dsi.initLatency(lat, jit, ref, tau)
 dsi.initContrast(th, th, th_noise)
-init_bgn_hist_cpp("D:/2023/computional imaging/my_pbrt/IEBCS-main/data/noise_neg_161lux.npy", "D:/2023/computional imaging/my_pbrt/IEBCS-main/data/noise_neg_161lux.npy")
+#the noise can also be better!
+init_bgn_hist_cpp("D:/2023/computional imaging/my_pbrt/IEBCS-main/data/noise_pos_161lux.npy", "D:/2023/computional imaging/my_pbrt/IEBCS-main/data/noise_pos_161lux.npy")
 isInit = False
 # dt = 1000  # FPS must be 1 kHz,this means fps = dt, and it shows that it can capture high fpx, like bird wings.
            #however, I do not need so high frequency, if I can't output somany pitures
 # if dt is too high, result is wrong? 
 # if bigger than 2857*2, result is wrong, I don't know why
 # if try to look better, may be small dt in 360...
-dt = 2857*6
+#if you change dt, t total is longer, that's right!
+dt = 2857
 ev_full = EventBuffer(1)
 ed = EventDisplay("Events", width, height, dt)
 time = 0
